9657cb7055281c57265c7f89217ec50b824ea718
rainpaw <ethanrbayer@gmail.com>
2023-11-30 16:57:39 -0600

    Add Project

---
+++ src/main/java/net/rainpaw10/medievalmadness/MedievalMadnessDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rainpaw10.medievalmadness;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MedievalMadnessDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
