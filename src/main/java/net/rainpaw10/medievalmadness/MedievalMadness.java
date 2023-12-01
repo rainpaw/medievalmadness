dc908bd7b7bceb41914194f3bf02550e879ed52f
rainpaw <ethanrbayer@gmail.com>
2023-12-01 16:21:39 -0600

    Add test item - ruby

--- src/main/java/net/rainpaw10/medievalmadness/MedievalMadness.java
+++ src/main/java/net/rainpaw10/medievalmadness/MedievalMadness.java
@@ -2,6 +2,7 @@ package net.rainpaw10.medievalmadness;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.rainpaw10.medievalmadness.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +15,8 @@ public class MedievalMadness implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		LOGGER.info("[" + MOD_ID + "]: Mod is initialized!");
 
-		LOGGER.info(MOD_ID + ": Mod is initialized!");
+		ModItems.registerModItems();
 	}
 }
